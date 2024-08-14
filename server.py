3dfc870300ac9c1d0b441881f6c12c41eeb7575a
Wingrayd <rino0611@ya.ru>
2024-08-14 05:57:35 +0300

    Update server.py
    Improved data reception process and format for future interface integration

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import socket
 import json
 import threading
+import multiprocessing
+import time
 
 def server_start():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -13,18 +15,46 @@ def client_connect(server_socket):
     while True:
         client_socket, client_address = server_socket.accept()
         print(f"Клиент подключился: {client_address}") 
-        thread = threading.Thread(target=monitoring, args=(client_socket, server_socket, client_address))
+        thread = threading.Thread(target=monitoring, args=(client_socket, client_address))
         thread.start()
 
-def monitoring(client_socket, server_socket, client_address):
+def monitoring(client_socket, client_address):
     addrs = ['192.168.1.169', '192.168.1.170', '192.168.1.171']
     json_addrs = json.dumps(addrs)
     client_socket.send(json_addrs.encode())
+    host = client_address[0]
     while True:
-        data = client_socket.recv(1024)
-        print(data.decode())
-        if not data:
-            print(f"Клиент отключился: {client_address}")
-            break
+        global problems
+        if host not in problems:
+            problems[host] = []
+            print(problems)
+        else:
+            data = client_socket.recv(1024)
+            if not data:
+                print(f"Клиент отключился: {client_address}")
+                problems.pop(host)
+                print(problems)
+                break
+            silent = json.loads(data.decode())
+            if silent != 47:
+                problems[host] = (silent)
+                print(problems)
+            
 
-server_start()
+################################################################################
+
+
+def test():
+    while True:
+        global problems
+    #    print(problems)
+        time.sleep(1)
+
+
+problems = {}
+server_play = multiprocessing.Process(target=server_start)
+interface = multiprocessing.Process(target=test)
+server_play.start()
+interface.start()
+server_play.join()
+interface.join()
