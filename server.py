ad1a2778f5a5eb79fea27a9ea753e0c1a1364c30
Wingrayd <rino0611@ya.ru>
2024-08-27 07:17:44 +0300

    Update server.py

--- server.py
+++ server.py
@@ -2,24 +2,33 @@ import socket
 import json
 import threading
 import multiprocessing
+from multiprocessing import Queue
+import mysql.connector
 import time
 
-def server_start():
+def server_start(queue):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind(('0.0.0.0', 30000))
     server_socket.listen(40)
     print("Сервер запущен и ожидает подключений...")
-    client_connect(server_socket)
+    client_connect(server_socket, queue)
 
-def client_connect(server_socket):
+def client_connect(server_socket, queue):
     while True:
         client_socket, client_address = server_socket.accept()
         print(f"Клиент подключился: {client_address}") 
-        thread = threading.Thread(target=monitoring, args=(client_socket, client_address))
+        database = mysql.connector.connect(user='root', password='7702', host='localhost', port='3306', database='pinger')
+        cursor = database.cursor()
+        query = (f"SELECT DISTINCT t.IP FROM DCTargets t INNER JOIN DCServers s ON t.id = s.id WHERE s.IP = '{client_address[0]}';")
+        cursor.execute(query)
+        addrs = cursor.fetchall()
+        cursor.close()
+        database.close()
+        thread = threading.Thread(target=monitoring, args=(client_socket, client_address, queue, addrs))
         thread.start()
 
-def monitoring(client_socket, client_address):
-    addrs = ['192.168.1.169', '192.168.1.170', '192.168.1.171']
+def monitoring(client_socket, client_address, queue, addrs):
+    print(addrs)
     json_addrs = json.dumps(addrs)
     client_socket.send(json_addrs.encode())
     host = client_address[0]
@@ -27,33 +36,33 @@ def monitoring(client_socket, client_address):
         global problems
         if host not in problems:
             problems[host] = []
-            print(problems)
+            queue.put(problems)
         else:
             data = client_socket.recv(1024)
             if not data:
                 print(f"Клиент отключился: {client_address}")
                 problems.pop(host)
-                print(problems)
+                queue.put(problems)
                 break
             silent = json.loads(data.decode())
-            if silent != 47:
+            if silent != True:
                 problems[host] = (silent)
-                print(problems)
+                queue.put(problems)
             
 
 ################################################################################
 
 
-def test():
+def test(queue):
     while True:
-        global problems
-    #    print(problems)
-        time.sleep(1)
+        problems = queue.get()
+        print(problems)
 
 
 problems = {}
-server_play = multiprocessing.Process(target=server_start)
-interface = multiprocessing.Process(target=test)
+queue = Queue()
+server_play = multiprocessing.Process(target=server_start, args=(queue,))
+interface = multiprocessing.Process(target=test, args=(queue,))
 server_play.start()
 interface.start()
 server_play.join()
